257cfa09a7c3b90a1c7d525b52cf59190a7e9fd8
joseucarvajal <joseucarvajal@gmail.com>
2025-02-12 19:26:26 -0500

    first commit

---
+++ v1_tienda_variables_input_output/main.py
@@ -0,0 +1,4 @@
+nombre_producto = input("Ingrese el nombre del producto: ")
+costo_producto = input("Ingrese el costo del producto: ")
+
+print(f"El producto {nombre_producto} tiene un costo de {costo_producto}")
